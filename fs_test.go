3976da9bb6f90e3c06fd138d286b424eefa915f3
Jacek Szwec <szwec.jacek@gmail.com>
2021-02-01 23:47:11 -0500

    Add extra mock test for readdir

--- fs_test.go
+++ fs_test.go
@@ -573,6 +573,233 @@ func TestFS(t *testing.T) {
 	}
 }
 
+func TestDirRead(t *testing.T) {
+	type fileinfo struct {
+		name  string
+		isDir bool
+	}
+
+	tests := []struct {
+		desc     string
+		n        int
+		outs     []s3.ListObjectsOutput
+		expected [][]fileinfo
+	}{
+		{
+			desc: "all in one request - dir first",
+			n:    1,
+			outs: []s3.ListObjectsOutput{
+				newListOutput([]string{"a", "c", "e"}, []string{"b", "d", "f"}),
+			},
+			expected: [][]fileinfo{
+				{{"a", true}},
+				{{"b", false}},
+				{{"c", true}},
+				{{"d", false}},
+				{{"e", true}},
+				{{"f", false}},
+			},
+		},
+		{
+			desc: "all in one request - n = 0",
+			n:    0,
+			outs: []s3.ListObjectsOutput{
+				newListOutput([]string{"a", "c", "e"}, []string{"b", "d", "f"}),
+			},
+			expected: [][]fileinfo{
+				{
+					{"a", true},
+					{"b", false},
+					{"c", true},
+					{"d", false},
+					{"e", true},
+					{"f", false},
+				},
+			},
+		},
+		{
+			desc: "all in one request - n = 2",
+			n:    2,
+			outs: []s3.ListObjectsOutput{
+				newListOutput([]string{"a"}, nil),
+				newListOutput([]string{"c"}, []string{"b", "d"}),
+				newListOutput([]string{"e"}, nil),
+				newListOutput(nil, []string{"f"}),
+			},
+			expected: [][]fileinfo{
+				{
+					{"a", true},
+					{"b", false},
+				},
+				{
+					{"c", true},
+					{"d", false},
+				},
+				{
+					{"e", true},
+					{"f", false},
+				},
+			},
+		},
+		{
+			desc: "one per request - dir first",
+			n:    1,
+			outs: []s3.ListObjectsOutput{
+				newListOutput([]string{"a"}, nil),
+				newListOutput(nil, []string{"b"}),
+				newListOutput([]string{"c"}, []string{"d"}),
+				newListOutput([]string{"e"}, nil),
+				newListOutput(nil, []string{"f"}),
+			},
+			expected: [][]fileinfo{
+				{{"a", true}},
+				{{"b", false}},
+				{{"c", true}},
+				{{"d", false}},
+				{{"e", true}},
+				{{"f", false}},
+			},
+		},
+		{
+			desc: "all in one request - file first",
+			n:    1,
+			outs: []s3.ListObjectsOutput{
+				newListOutput([]string{"b", "d", "f"}, []string{"a", "c", "e"}),
+			},
+			expected: [][]fileinfo{
+				{{"a", false}},
+				{{"b", true}},
+				{{"c", false}},
+				{{"d", true}},
+				{{"e", false}},
+				{{"f", true}},
+			},
+		},
+		{
+			desc: "with dir duplicates",
+			n:    1,
+			outs: []s3.ListObjectsOutput{
+				newListOutput([]string{"a", "c"}, []string{"b"}),
+				newListOutput([]string{"c", "e", "c"}, []string{"d"}),
+				newListOutput([]string{"e", "a"}, []string{"f"}),
+			},
+			expected: [][]fileinfo{
+				{{"a", true}},
+				{{"b", false}},
+				{{"c", true}},
+				{{"d", false}},
+				{{"e", true}},
+				{{"f", false}},
+			},
+		},
+
+		{
+			desc: "all in one request - dirs only",
+			n:    1,
+			outs: []s3.ListObjectsOutput{
+				newListOutput([]string{"a", "c", "e"}, nil),
+			},
+			expected: [][]fileinfo{
+				{{"a", true}},
+				{{"c", true}},
+				{{"e", true}},
+			},
+		},
+		{
+			desc: "single dir per request - dirs only",
+			n:    1,
+			outs: []s3.ListObjectsOutput{
+				newListOutput([]string{"a"}, nil),
+				newListOutput([]string{"c"}, nil),
+				newListOutput([]string{"e"}, nil),
+			},
+			expected: [][]fileinfo{
+				{{"a", true}},
+				{{"c", true}},
+				{{"e", true}},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			f, err := s3fs.New(&mockClient{
+				outs: test.outs,
+			}, "test").Open(".")
+			if err != nil {
+				t.Fatal("expected err to be nil; got ", err)
+			}
+
+			fi, err := f.Stat()
+			if err != nil {
+				t.Fatal("expected err to be nil; got ", err)
+			}
+
+			if !fi.IsDir() {
+				t.Fatal("expected the file to be a directory")
+			}
+
+			var fis [][]fileinfo
+			for {
+				files, err := f.(fs.ReadDirFile).ReadDir(test.n)
+				if err != nil && !errors.Is(err, io.EOF) {
+					t.Fatal("did not expect err:", err)
+				}
+
+				if len(files) > 0 {
+					var out []fileinfo
+					for _, f := range files {
+						out = append(out, fileinfo{f.Name(), f.IsDir()})
+					}
+					fis = append(fis, out)
+				}
+
+				if test.n <= 0 || errors.Is(err, io.EOF) {
+					break
+				}
+			}
+
+			if !reflect.DeepEqual(fis, test.expected) {
+				t.Errorf("want %v; got %v", test.expected, fis)
+			}
+		})
+	}
+}
+
+type mockClient struct {
+	s3iface.S3API
+	outs []s3.ListObjectsOutput
+	i    int
+}
+
+func (c *mockClient) ListObjects(in *s3.ListObjectsInput) (*s3.ListObjectsOutput, error) {
+	defer func() { c.i++ }()
+	if c.i < len(c.outs) {
+		return &c.outs[c.i], nil
+	}
+
+	return &s3.ListObjectsOutput{
+		IsTruncated: aws.Bool(false),
+	}, nil
+}
+
+func newListOutput(dirs, files []string) (out s3.ListObjectsOutput) {
+	for _, d := range dirs {
+		out.CommonPrefixes = append(out.CommonPrefixes, &s3.CommonPrefix{
+			Prefix: aws.String(d),
+		})
+	}
+
+	for _, f := range files {
+		out.Contents = append(out.Contents, &s3.Object{
+			Key:          aws.String(f),
+			Size:         aws.Int64(0),
+			LastModified: aws.Time(time.Time{}),
+		})
+	}
+	return out
+}
+
 func newClient(t *testing.T) s3iface.S3API {
 	t.Helper()
 
